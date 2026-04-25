eacc790820536eea67b5d1890c233f32181c3873
moemenmal <88335898+moemenmal@users.noreply.github.com>
2026-04-25 23:03:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  base: '/Profmoumen/';
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Profmoumen/',
     plugins: [react(), tailwindcss()],
     
     define: {
