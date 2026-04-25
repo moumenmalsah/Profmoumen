154061ba1b495e33858cdd469efa0727c4a42f48
moemenmal <88335898+moemenmal@users.noreply.github.com>
2026-04-25 23:00:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/Profmoumen/';
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
